71a0574eb8d56eb2d9b1ac15667ca17db504c1b4
dev-waqar <waansari2001@gmail.com>
2023-03-21 22:41:03 +0530

    initail start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tic-tac-toe';
+count=0;
+  title: any;
   ngOnInit(){
+    // let a=321
+    // console.log(a);
+    // for (let i = 0; i < this.winningCondition.length; i++) {
+    //   const element = this.winningCondition[i];
+    //   console.log(element);
+      
     
+    // }
+    // if (this.winningCondition.includes(321)) {
+    //   console.log('true');
+   
+      
+      
+    // }
+
+  }
+
+  winningCondition=[123,231,321,456,564,654,789,897,978,147,471,714,258,582,825,369,693,936,159,591,915,357,573,735]
+
+
+
+  termCheck(){
+
   }
 
 }
