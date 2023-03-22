4c38681941f5a4fd6e3908f12d3206e7c656c57b
dev-waqar <waansari2001@gmail.com>
2023-03-22 23:30:50 +0530

    ui .and function added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { count, map, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,34 +8,93 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-count=0;
+count1=0;
   title: any;
+  xtype!: string;
+  ytype!: string;
+ 
+
+//create observable that emits click events
+
   ngOnInit(){
-    // let a=321
-    // console.log(a);
-    // for (let i = 0; i < this.winningCondition.length; i++) {
-    //   const element = this.winningCondition[i];
-    //   console.log(element);
+    let a=321
+    console.log(a);
+    for (let i = 0; i < this.winningCondition.length; i++) {
+      const element = this.winningCondition[i];
+      // console.log(element);
       
     
-    // }
-    // if (this.winningCondition.includes(321)) {
-    //   console.log('true');
-   
-      
-      
-    // }
+    }
+    if (this.winningCondition.includes(a)) {
+      console.log('true');  
+    }
 
   }
 
   winningCondition=[123,231,321,456,564,654,789,897,978,147,471,714,258,582,825,369,693,936,159,591,915,357,573,735]
+  countData=['1','2','3','4','5','6','7','8','9']
+  countNumber:any = []
+  terms:string |undefined
+  XData:any=[]
+  YData:any=[]
 
 
 
-  termCheck(){
+  termCheck(el:any){
+    this.countNumber.push(el)
+    // console.log(this.countNumber.length);
+    if (this.countNumber.length%2==0) {
+      this.terms='X'
+      console.log(this.terms);
+      this.XData.push(el)
+      if (this.XData.length==3) {
+      console.log(this.XData.join(''));
+      this.xtype=(typeof Number(this.XData));
+      this.winnerTest()
 
-  }
+       
+      }
+    }
+    else{
+      this.terms='O'
+      console.log(this.terms);
+      if (this.YData.length<3) {
 
-}
+      this.YData.push(el)
+      console.log(this.YData.join(''));
+      this.ytype=(typeof Number(this.YData));
+
+      this.winnerTest()
+
+      } }
+
+    
+
+   
+    }
+
+    winnerTest(){
+        for (let i = 0; i < this.winningCondition.length; i++) {
+          const element = this.winningCondition[i];
+          // console.log(element);
+        }
+        if (this.winningCondition.includes(this.XData)) {
+          console.log('x is winner');
+          
+          
+        }
+        
+          if (this.winningCondition.includes(this.YData)) {
+            console.log('y is winner');
+            }
+          // else{
+          //   console.log('draw');
+            
+          // }
+        
+      }
+    }
+
+
 
 
